b6a4ba522cffb139e294e044af40bd7b48b82106
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-01-27 21:04:38 -0500

    set up all routes AND models

---
+++ models/User.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
+
+// define table columns and configuration
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+            isEmail: true
+        }
+    },
+    // define a password column
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            // this means the password must be at least four characters long
+            len: [4]
+        }
+    }
+
+}, {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    // pass in our imported sequelize connection 
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing 
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+});
+
+module.exports = User;
